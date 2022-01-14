4c78b88c4d9b471a859b73a6864fd36cf4eb9781
dindachyfr <dindachyfr@gmail.com>
2022-01-14 21:20:48 +0700

    nambah pokoknya

--- src/Context/WalletContext.js
+++ src/Context/WalletContext.js
@@ -1,35 +1,35 @@
-import axios from 'axios'
-import React, { createContext, useEffect, useState } from 'react'
+// import axios from 'axios'
+// import React, { createContext, useEffect, useState } from 'react'
 
-export const walletContext = createContext(null)
+// export const walletContext = createContext(null)
 
-const WalletContext = ({children}) => {
-    const user = JSON.parse(localStorage.getItem('user'))
-    const [wallet, setWallet] = useState({
-        id: 0,
-        user_id: 0,
-        balance: 0
-    })
+// const WalletContext = ({children}) => {
+//     const user = JSON.parse(localStorage.getItem('user'))
+//     const [wallet, setWallet] = useState({
+//         id: 0,
+//         user_id: 0,
+//         balance: 0
+//     })
 
-    useEffect(()=>{
-        axios.get(`https://zwallet-dinda.herokuapp.com/user-wallet/${user.wallet_id}`)
-        .then((res)=>{
-            const result = res.data.data[0]
-            setWallet(result)
-            localStorage.setItem('wallet', JSON.stringify(result))
-        })
-        .catch((err)=>{
-            console.log(err.response);
-        })
+//     useEffect(()=>{
+//         axios.get(`https://zwallet-dinda.herokuapp.com/user-wallet/${user.wallet_id}`)
+//         .then((res)=>{
+//             const result = res.data.data[0]
+//             setWallet(result)
+//             localStorage.setItem('wallet', JSON.stringify(result))
+//         })
+//         .catch((err)=>{
+//             console.log(err.response);
+//         })
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    })
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//     })
 
-    return (
-        <walletContext.Provider value = {{wallet, setWallet}}>
-            {children}
-        </walletContext.Provider>
-        )
-}
+//     return (
+//         <walletContext.Provider value = {{wallet, setWallet}}>
+//             {children}
+//         </walletContext.Provider>
+//         )
+// }
 
-export default WalletContext
+// export default WalletContext
