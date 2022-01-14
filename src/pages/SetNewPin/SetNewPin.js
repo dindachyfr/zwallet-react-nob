8dfd5883e7afb1812cd65fc766212519c463d609
dindachyfr <dindachyfr@gmail.com>
2022-01-14 21:20:39 +0700

    pokoknya nambah

---
+++ src/pages/SetNewPin/SetNewPin.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Footer from '../../components/module/Footer/Footer'
+import Navbar from '../../components/module/Navbar/Navbar'
+import SidebarMenu from '../../components/module/SidebarMenu/SidebarMenu'
+import SetPinNew from '../../components/module/ChangePin/SetPinNew'
+
+const SetNewPin = () => {
+    return (
+        <main className="con-home container-fluid d-flex flex-column bg-light p-0 justify-content-between">
+        <Navbar/>  
+        <main class="wrapper-content-home d-flex my-3 mx-lg-5 flex-fill">
+        <div class="container-content-home h-100 w-100 d-flex">
+            <SidebarMenu/>
+            <SetPinNew/>                        
+        </div>                    
+        </main>
+        <Footer/>
+    </main>
+    )
+}
+
+export default SetNewPin
