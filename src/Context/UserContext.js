45f908d73e2e9fce0ca382de19e061f22985a69f
dindachyfr <dindachyfr@gmail.com>
2022-01-17 11:47:03 +0700

    add use context

---
+++ src/Context/UserContext.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
+
+export const userContext = createContext(null)
+
+const UserContext = ({children}) => {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const [profile, setProfile] = useState({
+        id: 0,
+        name: "",
+        phone_number: "",
+        email: "",
+        pin: "",
+        wallet_id: 0,
+        balance: 0
+    })
+
+    useEffect (()=>{
+        if(user){
+        axios.get(`https://zwallet-dinda.herokuapp.com/users/${user.id}`)
+        .then((res)=>{
+            const result = res.data.data[0]
+            setProfile(result)
+        }).catch((err)=>{
+            console.log(err.response);
+
+        })
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <userContext.Provider value = {{profile, setProfile}}>
+            {children}
+        </userContext.Provider>
+    )
+}
+
+export default UserContext
